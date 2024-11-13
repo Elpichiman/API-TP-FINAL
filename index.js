5180b412a080b921b9bcbece65325c299b287a30
Pichiman <facundopichiman@gmail.com>
2024-11-13 20:25:28 -0300

    agregando endpoints

--- index.js
+++ index.js
@@ -68,6 +68,32 @@ app.post('/AgregarAerolinea', (req, res) => {
   });
 });
 
+// Modificar datos de una aerolínea
+app.put('/ModificarAerolinea/:id', (req, res) => {
+  const datos = leerDatos();
+  const id = parseInt(req.params.id);
+  const aerolinea = datos.aerolineas.find((aerolinea) => aerolinea.id === id);
+  
+  if (aerolinea) {
+    const nuevosDatosAerolínea = req.body;
+    aerolinea.nombre = nuevosDatosAerolínea.nombre;
+    aerolinea.cant_aviones = nuevosDatosAerolínea.cant_aviones;
+    aerolinea.pais = nuevosDatosAerolínea.pais;
+    aerolinea.alcance_mundo = nuevosDatosAerolínea.alcance_mundo;
+
+
+    fs.writeFile('./db.json', JSON.stringify(datos, null, 2), (err) => {
+      if (err) {
+        return res.status(500).send('Error al guardar el archivo.');
+      }
+      res.status(200).json(aerolinea);
+    });
+  } else {
+    res.status(404).send('Aerolínea no encontrada.');
+  }
+});
+
+
 // Pasajeros
 app.post('/SubirPasajero', (req, res) => {
   const nuevoPasajero = req.body;  
@@ -95,6 +121,46 @@ app.get('/ListarPasajeros', (req, res) => {
   const datos = leerDatos();
   res.json(datos.pasajeros);
 });
+
+// Buscar pasajero por ID
+app.get('/BuscarPasajero/:id', (req, res) => {
+  const datos = leerDatos();
+  const id = parseInt(req.params.id);
+  const pasajero = datos.pasajeros.find((pasajero) => pasajero.id === id);
+  
+  if (pasajero) {
+    res.json(pasajero);
+  } else {
+    res.status(404).send('Pasajero no encontrado.');
+  }
+});
+
+// Modificar datos del pasajero
+app.put('/ModificarPasajero/:id', (req, res) => {
+  const datos = leerDatos();
+  const id = parseInt(req.params.id);
+  const pasajero = datos.pasajeros.find((pasajero) => pasajero.id === id);
+  
+  if (pasajero) {
+    const nuevosDatosPasajero = req.body;
+    pasajero.nombre = nuevosDatosPasajero.nombre;
+    pasajero.apellido = nuevosDatosPasajero.apellido;
+    pasajero.dni = nuevosDatosPasajero.dni;
+    pasajero.contraseña = nuevosDatosPasajero.contraseña;
+    pasajero.usuario = nuevosDatosPasajero.usuario;
+    pasajero.mediosDePago = nuevosDatosPasajero.mediosDePago;
+
+    fs.writeFile('./db.json', JSON.stringify(datos, null, 2), (err) => {
+      if (err) {
+        return res.status(500).send('Error al guardar el archivo.');
+      }
+      res.status(200).json(pasajero);
+    });
+  } else {
+    res.status(404).send('Pasajero no encontrado.');
+  }
+});
+
 //Consulta de vuelo
 app.get('/ListarVuelos', (req, res) => {
   const datos = leerDatos();
@@ -143,6 +209,28 @@ app.post('/AgregarVuelo', (req, res) => {
   });
 });
 
+// Modificar datos de un vuelo
+app.put('/ModificarVuelo/:id', (req, res) => {
+  const datos = leerDatos();
+  const id = parseInt(req.params.id);
+  const vuelo = datos.vuelos.find((vuelo) => vuelo.id === id);
+  
+  if (vuelo) {
+    const nuevosDatosVuelo = req.body;
+    vuelo.lugar_salida = nuevosDatosVuelo.lugar_salida;
+    vuelo.lugar_llegada = nuevosDatosVuelo.lugar_llegada;
+    vuelo.horario = nuevosDatosVuelo.horario;
+
+    fs.writeFile('./db.json', JSON.stringify(datos, null, 2), (err) => {
+      if (err) {
+        return res.status(500).send('Error al guardar el archivo.');
+      }
+      res.status(200).json(vuelo);
+    });
+  } else {
+    res.status(404).send('Vuelo no encontrado.');
+  }
+});
 
 // Asientos
 app.get('/ListarAsientos', (req, res) => {
@@ -188,6 +276,71 @@ app.post('/AgregarAsiento', (req, res) => {
           res.status(201).json(nuevoAsiento);  
       });
   });
+});
+// Actualizar datos de un asiento
+app.put('/ActualizarAsiento/:id', (req, res) => {
+  const datos = leerDatos();
+  const id = parseInt(req.params.id);
+  const asiento = datos.asientos.find((asiento) => asiento.id === id);
+  if (asiento) {
+    const nuevoAsiento = req.body;
+    asiento.numero = nuevoAsiento.numero;
+    asiento.tipo = nuevoAsiento.tipo;
+    asiento.estado = nuevoAsiento.estado;
+    fs.writeFile('./db.json', JSON.stringify(datos, null, 2), (err) =>
+      {
+        if (err) {
+          return res.status(500).send('Error al guardar el archivo.');
+          }
+          res.status(200).json(asiento);
+          }
+          );
+          } else {
+            res.status(404).send('El asiento no existe.');
+            }
+            });
+// Eliminar un asiento
+app.delete('/EliminarAsiento/:id', (req, res) => {
+const datos = leerDatos();
+const id = parseInt(req.params.id);
+const asiento = datos.asientos.find((asiento) => asiento.id === id);
+if (asiento) {
+datos.asientos = datos.asientos.filter((asiento) => asiento.id !== id);
+fs.writeFile('./db.json', JSON.stringify(datos, null, 2), (err) =>
+{
+if (err) {
+return res.status(500).send('Error al guardar el archivo.');
+}
+res.status(200).json(asiento);
+}
+);
+} else {
+res.status(404).send('El asiento no existe.');
+}
+});
+
+// Modificar estado del asiento
+app.put('/ModificarEstadoAsiento/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const datos = leerDatos();
+  const asiento = datos.asientos.find((asiento) => asiento.id === id);
+
+  if (asiento) {
+    // Cambiar el estado del asiento
+    asiento.estado = asiento.estado === 'disponible' ? 'no disponible' : 'disponible';
+
+    const dbPath = './db.json';
+
+    // Guardar los cambios en el archivo
+    fs.writeFile(dbPath, JSON.stringify(datos, null, 2), (err) => {
+      if (err) {
+        return res.status(500).send('Error al guardar el archivo.');
+      }
+      res.json(asiento); // Retornar el asiento modificado
+    });
+  } else {
+    res.status(404).send("Asiento no encontrado.");
+  }
 });
 
 app.listen(port, () => {
